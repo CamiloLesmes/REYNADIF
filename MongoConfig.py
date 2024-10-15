7d10ae39089b821056e91a76220a77a050509cfa
CamiloLesmes <126630687+CamiloLesmes@users.noreply.github.com>
2024-10-15 17:23:01 -0500

    Update MongoConfig.py

--- MongoConfig.py
+++ MongoConfig.py
@@ -1,5 +1,5 @@
 config = {
     "URI":"mongodb+srv://cristianlesmes2003:<db_password>@cluster0.s1ywa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    "DATABASE":"CARNALITOMVP,
+    "DATABASE":"CARNALITOMVP",
     "COLLECTION":"CARNALITOMVP.ACE"
 }
